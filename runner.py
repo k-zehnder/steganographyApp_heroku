a156464161f6f9fee445c22a57978389cf1f0bdf
kevin <righttrianglesrkewl3@gmail.com>
2021-08-06 17:44:00 -0700

    new try

--- runner.py
+++ runner.py
@@ -6,7 +6,7 @@ app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Patient': ImageFile}
+    return {'db': db, 'ImageFile': ImageFile}
 
 if __name__ == '__main__':
     app.run(debug=True)
